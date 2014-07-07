a91a518720c57478f436bf23ecfd5586743273df
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-07-07 22:24:38 +0100

    new task modal

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,40 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+.controller('starterCtrl', function ($scope, $ionicModal) {
+    // $scope.tasks = [
+    //   { title: 'Collect coins' },
+    //   { title: 'Eat mushrooms' },
+    //   { title: 'Get high enough to grab the flag' },
+    //   { title: 'Find the Princess' }
+    // ];
+    $scope.tasks = [];
+
+    $ionicModal.fromTemplateUrl(
+      'new-task.html', 
+      function (modal) {
+        $scope.taskModal = modal;
+      }, {
+        scope : $scope,
+        animation : 'slide-in-up'
+      }
+    );
+
+    $scope.createTask = function (task) {
+      $scope.tasks.push({ title : task.title });
+      $scope.taskModal.hide();
+      task.title = '';
+    };
+
+    $scope.newTask = function () {
+      $scope.taskModal.show();
+    };
+
+    $scope.closeNewTask = function () {
+      $scope.taskModal.hide();
+    }
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
